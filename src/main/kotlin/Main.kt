51c7ad6dccbc3056cb4ee1f55248e693f99b60bb
Android61 <androidalek@yandex.ru>
2023-10-16 22:22:07 +0300

    git add

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+fun main() {
+    val likes = 2
+    var endMessage = ""
+
+    if (likes % 10 == 1) {
+        endMessage = "человеку"
+    }
+        else endMessage = "людям"
+
+        println("Понравилось $likes $endMessage.")
+
+
+
+    }
+
+
